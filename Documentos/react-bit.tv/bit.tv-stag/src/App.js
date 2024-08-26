9edd48febfb2aeffa4e005d19cd0d4f124e31305
Gabriel França <menunoventaecinco@gmail.com>
2024-08-26 14:06:05 -0300

    Modificações em App.js, HTML e outros componentes

--- Documentos/react-bit.tv/bit.tv-stag/src/App.js
+++ Documentos/react-bit.tv/bit.tv-stag/src/App.js
@@ -1,25 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import ChannelList from "./components/ChannelList";
+import VideoPlayer from "./components/VideoPlayer";
+import "./styles.css";
 
-function App() {
+const App = () => {
+  const [channels, setChannels] = useState([
+    { name: "Channel 1", src: "source1.m3u8", type: "m3u8", isSelected: false },
+    { name: "Channel 2", src: "source2.m3u8", type: "m3u8", isSelected: false },
+    // Outros canais...
+  ]);
+
+  const [currentChannelIndex, setCurrentChannelIndex] = useState(0);
+
+  const handleChannelSelect = (index) => {
+    const updatedChannels = channels.map((channel, i) => ({
+      ...channel,
+      isSelected: i === index,
+    }));
+    setChannels(updatedChannels);
+    setCurrentChannelIndex(index);
+  };
+
+  useEffect(() => {
+    const handleKeyDown = (e) => {
+      if (e.key === "Enter" || e.key === "Backspace") {
+        // Lógica para abrir o menu de canais ou selecionar um canal
+      } else if (e.key === "ArrowDown") {
+        setCurrentChannelIndex((prevIndex) => {
+          const nextIndex = (prevIndex + 1) % channels.length;
+          handleChannelSelect(nextIndex);
+          return nextIndex;
+        });
+      } else if (e.key === "ArrowUp") {
+        setCurrentChannelIndex((prevIndex) => {
+          const prevChannelIndex =
+            (prevIndex - 1 + channels.length) % channels.length;
+          handleChannelSelect(prevChannelIndex);
+          return prevChannelIndex;
+        });
+      }
+    };
+
+    document.addEventListener("keydown", handleKeyDown);
+    return () => {
+      document.removeEventListener("keydown", handleKeyDown);
+    };
+  }, [channels]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <ChannelList channels={channels} onSelect={handleChannelSelect} />
+      <VideoPlayer
+        src={channels[currentChannelIndex].src}
+        type={channels[currentChannelIndex].type}
+      />
     </div>
   );
-}
+};
 
 export default App;
